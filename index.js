1be84b1742747d562be7e800a1ab786249c9aed4
sammun1926 <munorusam10@gmail.com>
2023-03-16 12:46:47 +0300

    completed asssignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+//random
+var random = Math.floor(Math.random()* 10) + 1;
+
+//remainder
+var num3 =12;
+var num4 = 8;
+var mod = num3 % num4;
+
+//highest number
+var max = Math.max(1,2,3,4,20);
